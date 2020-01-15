21b89d31a465e7189d05304b868eca27bce8606d
Leandro Marques <leandrolmarques@gmail.com>
2020-01-15 18:56:29 -0300

    Add a provider with default state to App

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import ISSLocation from './components/ISSLocation';
 const ISSContext = createContext();
 
 function App() {
+  const contextValue = {
+    error: null,
+    isFetching: false,
+    latitude: -19.9410656,
+    longitude: -43.9333033,
+  };
+
   return (
-    <div className="App">
-      <h1>International Space Station Location Tracker</h1>
-      <ISSLocation />
-    </div>
+    <ISSContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>International Space Station Location Tracker</h1>
+        <ISSLocation />
+      </div>
+    </ISSContext.Provider>
   );
 }
 
