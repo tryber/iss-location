9de4dcf522433f5cd779ff5d9e4575437d5ebc31
Leandro Marques <leandrolmarques@gmail.com>
2020-01-15 18:55:01 -0300

    Create a context to hold app state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
 import ISSLocation from './components/ISSLocation';
+
+const ISSContext = createContext();
 
 function App() {
   return (
