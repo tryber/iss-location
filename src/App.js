75438d2d206d6bd9c91acbc6cb82e6ad4f5f8584
Leandro Marques <leandrolmarques@gmail.com>
2020-01-15 19:54:50 -0300

    Convert App to class component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ISSLocation from './components/ISSLocation';
 import ISSContext from './context/ISSContext';
 
-function App() {
-  const contextValue = {
-    error: null,
-    isFetching: false,
-    latitude: -19.9410656,
-    longitude: -43.9333033,
-  };
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isFetching: false,
+      latitude: -19.9410656,
+      longitude: -43.9333033,
+    };
+  }
 
-  return (
-    <ISSContext.Provider value={contextValue}>
-      <div className="App">
-        <h1>International Space Station Location Tracker</h1>
-        <ISSLocation />
-      </div>
-    </ISSContext.Provider>
-  );
+  render() {
+    const contextValue = {
+      ...this.state,
+    };
+
+    return (
+      <ISSContext.Provider value={contextValue}>
+        <div className="App">
+          <h1>International Space Station Location Tracker</h1>
+          <ISSLocation />
+        </div>
+      </ISSContext.Provider>
+    );
+  }
 }
 
 export default App;
