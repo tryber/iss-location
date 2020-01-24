52ef497df84b7f9110f99fd84fbf4701ae9115e7
Leandro Marques <leandrolmarques@gmail.com>
2020-01-24 10:53:04 -0300

    Convert ISSLocation to function component

--- src/components/ISSLocation.js
+++ src/components/ISSLocation.js
@@ -1,60 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker';
 
 import ISSContext from '../context/ISSContext';
 
-class ISSLocation extends Component {
-  componentDidMount() {
-    const { getCurrentISSLocation } = this.context;
+function ISSLocation() {
+  // componentDidMount() {
+  //   const { getCurrentISSLocation } = this.context;
 
-    this.timer = setInterval(
-      getCurrentISSLocation,
-      2000,
-    );
+  //   this.timer = setInterval(
+  //     getCurrentISSLocation,
+  //     2000,
+  //   );
 
-    const now = new Date();
-    document.title = now.toLocaleTimeString();
-  }
+  //   const now = new Date();
+  //   document.title = now.toLocaleTimeString();
+  // }
 
-  componentDidUpdate() {
-    const now = new Date();
-    document.title = now.toLocaleTimeString();
-  }
+  // componentDidUpdate() {
+  //   const now = new Date();
+  //   document.title = now.toLocaleTimeString();
+  // }
 
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
+  // componentWillUnmount() {
+  //   clearInterval(this.timer);
+  // }
 
-  render() {
-    const {
-      error,
-      isFetching,
-      latitude,
-      longitude,
-    } = this.context;
-    const isLocationPresent = latitude && longitude;
+  const {
+    error,
+    isFetching,
+    latitude,
+    longitude,
+  } = this.context;
+  const isLocationPresent = latitude && longitude;
 
-    return (
-      <div>
-        <div className="map">
-          <Map
-            center={[0, 0]}
-            defaultWidth={700}
-            height={450}
-            minZoom={1}
-            maxZoom={20}
-            zoom={1}
-          >
-            {!isFetching && isLocationPresent && <Marker anchor={[latitude, longitude]} />}
-          </Map>
-        </div>
-        {isFetching && 'Loading...'}
-        {!isFetching && isLocationPresent && `Current ISS location: latitude = ${latitude}, longitude = ${longitude}`}
-        {!isFetching && error}
+  return (
+    <div>
+      <div className="map">
+        <Map
+          center={[0, 0]}
+          defaultWidth={700}
+          height={450}
+          minZoom={1}
+          maxZoom={20}
+          zoom={1}
+        >
+          {!isFetching && isLocationPresent && <Marker anchor={[latitude, longitude]} />}
+        </Map>
       </div>
-    );
-  }
+      {isFetching && 'Loading...'}
+      {!isFetching && isLocationPresent && `Current ISS location: latitude = ${latitude}, longitude = ${longitude}`}
+      {!isFetching && error}
+    </div>
+  );
 }
 
 ISSLocation.contextType = ISSContext;
